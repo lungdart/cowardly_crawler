92ecffb33de4e545ecec28d85889121c42db9bd7
Shawn Boutilier <shawn@lungdart.net>
2020-05-28 10:26:12 -0300

    * Fixed bug that allowed the mouse cursor to trigger doors.

--- src/level/Door.gd
+++ src/level/Door.gd
@@ -5,6 +5,7 @@ export var POSITION = Vector2.ZERO
 
 
 func _on_Area2D_body_entered(body):
-	if POSITION != Vector2.ZERO:
-		GlobalState.player_position = POSITION
-	SceneChanger.change_scene(LEVEL_PATH)
+	if body.name == "Player":
+		if POSITION != Vector2.ZERO:
+			GlobalState.player_position = POSITION
+		SceneChanger.change_scene(LEVEL_PATH)
